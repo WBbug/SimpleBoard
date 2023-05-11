cceac9bb2edb06f5c3e6dffe297699b55db2faac
WBbug <1056342711@qq.com>
2023-05-12 00:03:28 +0800

    feat(core): init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+    <TheWelcome />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  hello world
+</template>
